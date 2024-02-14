36e5521c11018dac0c82b92669447002f00f1b6c
Litwesh <104084923+Litwesh@users.noreply.github.com>
2024-02-14 17:20:25 +0400

    Add files via upload

---
+++ PRO-C127/old_scraper.py
@@ -0,0 +1,57 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from bs4 import BeautifulSoup
+import time
+import pandas as pd
+
+# NASA Exoplanet URL
+START_URL = "https://en.wikipedia.org/w/index.php?title=List_of_brightest_stars_and_other_record_stars&oldid=1187363889"
+
+# Webdriver
+browser = webdriver.Edge("D:\Downloads\PRO-C127\msedgedriver.exe")
+browser.get(START_URL)
+
+time.sleep(10)
+
+scraped_data = []
+
+# Define Exoplanet Data Scrapping Method
+def scrape():
+    soup=BeautifulSoup(browser.page_source,"html.parser")
+    bright_star_table = soup.find("table",attrs=("class","wikitable sortable jquery-tablesorter"))
+    table_body=bright_star_table.find('tbody')
+    table_rows=table_body.find_all('tr')
+    for row in table_rows:
+
+        table_cols = row.find_all("td")
+        print(table_cols)
+        temp_list = []
+
+        for col_data in table_cols:
+            data=col_data.text.strip()
+            #print(data)
+            temp_list.append(data)
+        scraped_data.append(temp_list)
+scrape()
+stars_data=[]
+print(scraped_data)
+for i in range(0,len(scraped_data)):
+    Star_names=scraped_data[i][1]
+    Distance=scraped_data[i][3]
+    Mass=scraped_data[i][5]
+    Radius=scraped_data[i][6]
+    Lum=scraped_data[i][7]    
+
+    required_data = [Star_names,Distance,Mass,Radius,Lum]
+    stars_data.append(required_data)    
+print(stars_data)
+
+
+# Define Header
+headers = ["Star_name", "Distance", "Mass", "Radius", "Luminosity"]
+
+# Define pandas DataFrame   
+star_df_1 = pd.DataFrame(stars_data, columns=headers)
+
+# Convert to CSV
+star_df_1.to_csv('scraped_data.csv',index=True, index_label="id")
